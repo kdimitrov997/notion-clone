63eb9a8675d25aa09a7c40a4c7b44f022c150707
Krasimir Dimitrov <k.dimitrov997@gmail.com>
2023-11-02 21:23:13 +0200

    Update

---
+++ app/(main)/_components/document-list.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { api } from "@/convex/_generated/api";
+import { Doc, Id } from "@/convex/_generated/dataModel";
+import { useQuery } from "convex/react";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+
+interface DocumentListProps {
+  parentDocumentId?: Id<"documents">;
+  level?: number;
+  data?: Doc<"documents">;
+}
+
+export const DocumentList = ({
+ parentDocumentId,
+ level = 0, 
+}: DocumentListProps) => {
+  const params = useParams();
+  const router = useRouter();
+  const [expanded, setExpanded] = useState<Record<string, boolean>>({});
+
+  const onExpand = (documentId: string) => {
+    setExpanded(prevExpanded => ({
+      ...prevExpanded,
+      [documentId]: !prevExpanded[documentId]
+    }));
+  };
+
+  const documents = useQuery(api.documents.getSidebar, {
+    parentDocument: parentDocumentId
+  });
+
+  const onRedirect = (documentId: string) => {
+    router.push(`/documents/${documentId}`);
+  };
+
+  return (
+    <div>
+      Document List
+    </div>
+  )
+}
--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { Id } from "@/convex/_generated/dataModel";
 import { cn } from "@/lib/utils";
 import { ChevronDown, ChevronRight, LucideIcon } from "lucide-react";
@@ -51,10 +52,31 @@ export const Item = ({
           <ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
         </div>
       )}
-      <Icon 
-        className="shrink-0 h-[18px] mr-2 text-muted-foreground" 
-      />
+      {documentIcon ? (
+        <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
+      ) : (
+        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+      )}
       <span className="truncate">{label}</span>
+      {isSearch && (
+        <kbd className="ml-auto pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      )}
     </div>
   );
 };
+
+Item.Skeleton = function ItemSkeleton({ level }: { level?: number }) {
+  return (
+    <div
+      style={{
+        paddingLeft: level ? `${(level * 12) + 25}px` : "12px"
+      }}
+      className="flex gap-x-2 py-[3px]"
+    >
+      <Skeleton className="h-4 w-4" />
+      <Skeleton className="h-4 w-[30%]" />
+    </div>
+  )
+}
